1a2c9bcdb43948160d8fe3a94f2d0894b52b7328
David H. Bronke <whitelynx@gmail.com>
2018-04-10 15:18:49 -0400

    Much better LineMapLayer now

--- LineMapLayer.py
+++ LineMapLayer.py
@@ -1,31 +1,20 @@
-from datetime import datetime
-import random
 from math import *
-from pprint import pprint
 
-from kivy.graphics import Color, Line, SmoothLine
+from kivy.graphics import Color, Line, SmoothLine, MatrixInstruction
 from kivy.graphics.context_instructions import Translate, Scale
 from kivy.clock import Clock
 
 from kivy.garden.mapview.mapview.utils import clamp
 from kivy.garden.mapview.mapview import MapLayer, MIN_LONGITUDE, MIN_LATITUDE, MAX_LATITUDE, MAX_LONGITUDE
 
-import googlemaps
-import polyline
-
-
-with open('google-maps-directions.apikey', 'r') as f:
-    mapsAPIKey = f.read()
-
 
 class LineMapLayer(MapLayer):
     def __init__(self, **kwargs):
         super(LineMapLayer, self).__init__(**kwargs)
         self._coordinates = []
+        self._line_points = None
+        self._line_points_offset = (0, 0)
         self.zoom = 0
-
-        self.add_nav_coords()
-        Clock.schedule_once(self.draw_line, 0)
 
     @property
     def coordinates(self):
@@ -34,61 +23,82 @@ class LineMapLayer(MapLayer):
     @coordinates.setter
     def coordinates(self, coordinates):
         self._coordinates = coordinates
+        self.invalidate_line_points()
+        self.clear_and_redraw()
 
-        #: Since lat is not a linear transform we must compute manually
-        self.line_points = [(self.get_x(lon), self.get_y(lat)) for lat, lon in coordinates]
-        #self.line_points = [mapview.get_window_xy_from(lat, lon, mapview.zoom) for lat, lon in coordinates]
+    @property
+    def line_points(self):
+        if self._line_points is None:
+            self.calc_line_points()
+        return self._line_points
 
-    def reposition(self):
-        mapview = self.parent
+    @property
+    def line_points_offset(self):
+        if self._line_points is None:
+            self.calc_line_points()
+        return self._line_points_offset
 
-        #: Must redraw when the zoom changes
-        #: as the scatter transform resets for the new tiles
-        if (self.zoom != mapview.zoom):
-            self.draw_line()
+    def calc_line_points(self):
+        # Offset all points by the coordinates of the first point, to keep coordinates closer to zero.
+        # (and therefore avoid some float precision issues when drawing lines)
+        self._line_points_offset = (self.get_x(self.coordinates[0][1]), self.get_y(self.coordinates[0][0]))
+        # Since lat is not a linear transform we must compute manually
+        self._line_points = [(self.get_x(lon) - self._line_points_offset[0], self.get_y(lat) - self._line_points_offset[1]) for lat, lon in self.coordinates]
 
-    def add_nav_coords(self):
-        gmaps = googlemaps.Client(key=mapsAPIKey)
-        now = datetime.now()
-        routes = gmaps.directions(
-            "100 broad st weymouth ma",
-            "rice passions malden ma",
-            mode="transit",
-            departure_time=now
-        )
-        self.coordinates = polyline.decode(routes[0]['overview_polyline']['points'])
+    def invalidate_line_points(self):
+        self._line_points = None
+        self._line_points_offset = (0, 0)
 
     def get_x(self, lon):
         '''Get the x position on the map using this map source's projection
         (0, 0) is located at the top left.
         '''
-        return clamp(lon, MIN_LONGITUDE, MAX_LONGITUDE) / 180.
+        return clamp(lon, MIN_LONGITUDE, MAX_LONGITUDE) * self.ms / 360.0
 
     def get_y(self, lat):
         '''Get the y position on the map using this map source's projection
         (0, 0) is located at the top left.
         '''
-        lat = clamp(-lat, MIN_LATITUDE, MAX_LATITUDE)
-        lat = lat * pi / 180.
-        return ((1.0 - log(tan(lat) + 1.0 / cos(lat)) / pi))
+        lat = radians(clamp(-lat, MIN_LATITUDE, MAX_LATITUDE))
+        return ((1.0 - log(tan(lat) + 1.0 / cos(lat)) / pi)) * self.ms / 2.0
 
-    def draw_line(self, *args):
+    def reposition(self):
+        mapview = self.parent
+
+        # Must redraw when the zoom changes
+        # as the scatter transform resets for the new tiles
+        if (self.zoom != mapview.zoom):
+            map_source = mapview.map_source
+            self.ms = pow(2.0, mapview.zoom) * map_source.dp_tile_size
+            self.invalidate_line_points()
+            self.clear_and_redraw()
+
+    def clear_and_redraw(self, *args):
+        with self.canvas:
+            # Clear old line
+            self.canvas.clear()
+
+        # FIXME: Why is 0.05 a good value here? Why does 0 leave us with weird offsets?
+        Clock.schedule_once(self._draw_line, 0.05)
+
+    def _draw_line(self, *args):
         mapview = self.parent
         self.zoom = mapview.zoom
 
         # When zooming we must undo the current scatter transform
         # or the animation distorts it
         scatter = mapview._scatter
-        map_source = mapview.map_source
         sx, sy, ss = scatter.x, scatter.y, scatter.scale
-        vx, vy, vs = mapview.viewport_pos[0], mapview.viewport_pos[1], mapview.scale
 
         # Account for map source tile size and mapview zoom
-        ms = pow(2.0, mapview.zoom) * map_source.dp_tile_size
+        vx, vy, vs = mapview.viewport_pos[0], mapview.viewport_pos[1], mapview.scale
 
         with self.canvas:
             # Clear old line
             self.canvas.clear()
+
+            # Offset by the MapView's position in the window
+            Translate(*mapview.pos)
 
             # Undo the scatter animation transform
             Scale(1 / ss, 1 / ss, 1)
@@ -98,18 +108,16 @@ class LineMapLayer(MapLayer):
             Scale(vs, vs, 1)
             Translate(-vx, -vy)
 
-            # Apply the what we can factor out
-            # of the mapsource long, lat to x, y conversion
-            Scale(ms / 2.0, ms / 2.0, 1)
-            Translate(1, 0)
+            # Apply the what we can factor out of the mapsource long, lat to x, y conversion
+            Translate(self.ms / 2, 0)
 
-            # Draw new
+            # Translate by the offset of the line points (this keeps the points closer to the origin)
+            Translate(*self.line_points_offset)
+
+            # Draw line
             Color(0, 0.2, 0.7, 0.25)
-            Line(points=self.line_points, width=6.5 / ms)
+            Line(points=self.line_points, width=6.5 / 2)
             Color(0, 0.2, 0.7, 1)
-            Line(points=self.line_points, width=6 / ms)
+            Line(points=self.line_points, width=6 / 2)
             Color(0, 0.3, 1, 1)
-            Line(points=self.line_points, width=4 / ms)
-            #Line(points=self.line_points, width=1)#4 / ms)#, joint='round', joint_precision=100)
-            #Line(points=self.line_points, width=4 / ms, joint='round', close=False, cap='none')
-            #Line(points=self.line_points, width=1)
+            Line(points=self.line_points, width=4 / 2)
--- MapPage.py
+++ MapPage.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import kivy
 kivy.require('1.10.0')
 
@@ -7,7 +9,14 @@ from kivy.properties import ListProperty, ObjectProperty, StringProperty
 
 from kivy.garden.mapview import MapSource
 
+import googlemaps
+import polyline
+
 from LineMapLayer import LineMapLayer
+
+
+with open('google-maps-directions.apikey', 'r') as f:
+    mapsAPIKey = f.read()
 
 
 class MapPage(BoxLayout):
@@ -16,5 +25,16 @@ class MapPage(BoxLayout):
         Clock.schedule_once(self.add_line_layer, 0)
 
     def add_line_layer(self, *args):
+        gmaps = googlemaps.Client(key=mapsAPIKey)
+        now = datetime.now()
+        routes = gmaps.directions(
+            '100 broad st weymouth ma',
+            'rice passions malden ma',
+            mode='driving',
+            departure_time=now
+        )
+
         line = LineMapLayer()
+        line.coordinates = polyline.decode(routes[0]['overview_polyline']['points'])
+
         self.ids.mapview.add_layer(line, mode='scatter')
