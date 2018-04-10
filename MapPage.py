93d30693f802b61366d55ae3b17d444ba9d2253b
David H. Bronke <whitelynx@gmail.com>
2018-04-10 18:34:08 -0400

    Real directions!

--- MapPage.py
+++ MapPage.py
@@ -3,8 +3,9 @@ from datetime import datetime
 import kivy
 kivy.require('1.10.0')
 
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 from kivy.clock import Clock
+from kivy.logger import Logger
 from kivy.properties import ListProperty, ObjectProperty, StringProperty
 
 from kivy.garden.mapview import MapSource
@@ -19,22 +20,37 @@ with open('google-maps-directions.apikey', 'r') as f:
     mapsAPIKey = f.read()
 
 
-class MapPage(BoxLayout):
+class MapPage(GridLayout):
     def __init__(self, **kwargs):
         super(MapPage, self).__init__(**kwargs)
-        Clock.schedule_once(self.add_line_layer, 0)
+        self._line_layer = None
+        self.gmaps = googlemaps.Client(key=mapsAPIKey)
 
-    def add_line_layer(self, *args):
-        gmaps = googlemaps.Client(key=mapsAPIKey)
-        now = datetime.now()
-        routes = gmaps.directions(
-            '100 broad st weymouth ma',
-            'rice passions malden ma',
-            mode='driving',
-            departure_time=now
-        )
+    @property
+    def line_layer(self):
+        if self._line_layer is None:
+            self._line_layer = LineMapLayer()
+            self.ids.mapview.add_layer(self._line_layer, mode='scatter')
 
-        line = LineMapLayer()
-        line.coordinates = polyline.decode(routes[0]['overview_polyline']['points'])
+        return self._line_layer
 
-        self.ids.mapview.add_layer(line, mode='scatter')
+    def get_directions(self, fromAddr, toAddr):
+        Logger.info(f'Getting directions from {fromAddr!r} to {toAddr!r}...')
+        try:
+            routes = self.gmaps.directions(
+                fromAddr,
+                toAddr,
+                mode='driving',
+                departure_time=datetime.now()
+            )
+
+            route = routes[0]
+            self.line_layer.coordinates = polyline.decode(route['overview_polyline']['points'])
+
+            bounds = route['bounds']
+            minBound = bounds['southwest']['lat'], bounds['southwest']['lng']
+            maxBound = bounds['northeast']['lat'], bounds['northeast']['lng']
+            self.ids.mapview.center_on((minBound[0] + maxBound[0]) / 2, (minBound[1] + maxBound[1]) / 2)
+
+        except googlemaps.exceptions.HTTPError as err:
+            Logger.warn(f'Error from Google Maps API: {err}')
