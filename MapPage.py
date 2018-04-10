937eee629db970a0419cdaa2eea0007650bb1899
David H. Bronke <whitelynx@gmail.com>
2018-04-10 03:08:27 +0000

    Added MapPage

---
+++ MapPage.py
@@ -0,0 +1,11 @@
+import kivy
+kivy.require('1.10.0')
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ListProperty, ObjectProperty, StringProperty
+
+from kivy.garden.mapview import MapSource
+
+
+class MapPage(BoxLayout):
+    pass
